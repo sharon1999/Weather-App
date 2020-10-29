57844fb10ab302fc0499d9c0a89d15c9fde58d23
Sharon Antony <sharonantony51@gmail.com>
2020-10-29 08:32:55 +0530

    Add Header section

--- my-weather-app/src/components/Header.js
+++ my-weather-app/src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import '../App.css';
+import WbSunnyIcon from '@material-ui/icons/WbSunny';
+import IconButton from '@material-ui/core/IconButton';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import TwitterIcon from '@material-ui/icons/Twitter';
+
 
 function Header(){
     return(
-        <div className="Head">
-            <h1>Weather App</h1>
-        </div>
+        <div className="Outer">
+            <div className="Head">
+                <IconButton>
+                    <TwitterIcon/>
+                </IconButton>  
+                <IconButton>
+                    <InstagramIcon/>
+                </IconButton>    
+                <h1>Weather App</h1>
+                <IconButton>
+                    <WbSunnyIcon color="primary" fontSize="large"  />
+                </IconButton>               
+            </div>
+        </div>    
     )
 }
 export default Header
